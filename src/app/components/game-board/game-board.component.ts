420d81ded68d4ea4b88dd9886dc1de7e58aabe07
Zvi Weiss <zvweiss@gmail.com>
2026-01-12 16:37:38 -0500

    more color tweaks

--- src/app/components/game-board/game-board.component.ts
+++ src/app/components/game-board/game-board.component.ts
@@ -23,7 +23,7 @@ export class GameBoardComponent {
     if (value === 0) {
       return 'tile tile-0'
     }
-      if (value < 2048) {
+      if (value <= 8192) {
       return 'tile tile-' + value
     }
     return 'tile tile.tile-super';
